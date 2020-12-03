6fcb12403fdef97b6982e3045eb5b1bcb4af9310
Kristen Collier <kristencollier10@gmail.com>
2020-12-03 14:36:55 -0600

    updates

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -27,12 +27,12 @@ public class ControlFlowExercises {
 //        }
 //        while (i > -10);
 
-        long i = 2;
-        do {
-            i = i * i;
-            System.out.println(i);
-        }
-        while (i <= 65537);
+//        long i = 2;
+//        do {
+//            i = i * i;
+//            System.out.println(i);
+//        }
+//        while (i <= 65537);
 
 //        for(int i = 5; i <= 15; i++) {
 //            System.out.println(i);
@@ -51,7 +51,18 @@ public class ControlFlowExercises {
 //        for(long i = 2; i <= 65536; i *= i){
 //            System.out.println(i);
 //        }
-
+//
+//        for(int i = 0; i <= 100; i++) {
+//            if(i % 15 == 0) {
+//                System.out.println("FizzBuzz");
+//            } else if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
 
 
     }
