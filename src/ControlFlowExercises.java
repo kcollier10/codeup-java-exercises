b31be4faab43e30b7273762c34c36966fac7e396
Kristen Collier <kristencollier10@gmail.com>
2020-12-03 14:07:51 -0600

    exercise started

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class ControlFlowExercises {
+    public static void main (String[] arg) {
+        Scanner myScanner = new Scanner(System.in);
+
+//        int i = 5;
+//
+//        while (i <= 15) {
+//            System.out.printf("%d ", i);
+//            i++;
+//        }
+//
+//        System.out.println("-----------------");
+
+//        int i = 1;
+//
+//        do {
+//            if(i % 2 == 0) {
+//                System.out.print(i + "\n");
+//            }
+//            i++;
+//        }
+//        while (i >= 100);
+
+
+    }
+}
