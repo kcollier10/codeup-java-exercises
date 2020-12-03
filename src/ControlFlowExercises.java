71e73175b37e5c79c52c63d0af52dfb9c7e6cdc9
Kristen Collier <kristencollier10@gmail.com>
2020-12-03 16:49:08 -0600

    updates

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -32,7 +32,7 @@ public class ControlFlowExercises {
 //            i = i * i;
 //            System.out.println(i);
 //        }
-//        while (i <= 65537);
+//        while (i <= 65537L);
 
 //        for(int i = 5; i <= 15; i++) {
 //            System.out.println(i);
@@ -44,7 +44,7 @@ public class ControlFlowExercises {
 //            }
 //        }
 
-//        for (var i = 100; i >= -10; i -= 5) {
+//        for (int i = 100; i >= -10; i -= 5) {
 //            System.out.println(i);
 //        }
 
@@ -63,7 +63,82 @@ public class ControlFlowExercises {
 //                System.out.println(i);
 //            }
 //        }
+        boolean userContinues = true;
 
+        do {
+
+            System.out.println("Please enter your max number:");
+            int number = myScanner.nextInt();
+            System.out.printf("You entered: %d.", number);
+            System.out.println();
+            System.out.printf("Here is your table, up to number: %d", number);
+            System.out.println();
+            System.out.println(" number |  squared  | cubed ");
+            System.out.println(" ------ |  -------  | ----- ");
+
+
+            for (int i = 1; i <= number; i++) {
+                i = number;
+                int numberSquared = (int) Math.pow(i, 2);
+                int numberCubed = (int) Math.pow(i, 3);
+
+                System.out.printf("%-7d", i);
+                System.out.print("|");
+                System.out.printf("%-8d", numberSquared);
+                System.out.print("|");
+                System.out.printf("%-9d", numberCubed);
+                System.out.println();
+            }
+
+            System.out.print("Would you like to enter another number? Y or N");
+            String userResponse = myScanner.next();
+            if(!userResponse.equalsIgnoreCase("y")) {
+                userContinues = false;
+            }
+        } while (userContinues);
+
+
+       do {
+           System.out.println("Enter an integer (whole number without digits):");
+           int grade = myScanner.nextInt();
+           if (grade <= 59) {
+               System.out.println("Current grade is a : F");
+           } else if (grade <= 66) {
+               if (66 - grade <= 2) {
+                   System.out.println("Current grade is a : D+");
+               } else if (66 - grade >= 6) {
+                   System.out.println("Current grade is a : D-");
+               } else
+                   System.out.println("Current grade is a : D");
+           } else if (grade <= 79) {
+               if (79 - grade <= 3) {
+                   System.out.println("Current grade is a : C+");
+               } else if (79 - grade >= 9) {
+                   System.out.println("Current grade is a : C-");
+               } else
+                   System.out.println("Current grade is a : C");
+           } else if (grade <= 87) {
+               if (87 - grade <= 3) {
+                   System.out.println("Current grade is a : B+");
+               } else if (87 - grade >= 6) {
+                   System.out.println("Current grade is a : B-");
+               } else
+                   System.out.println("Current grade is a : B");
+           } else if (grade <= 100) {
+               if (100 - grade <= 5) {
+                   System.out.println("Current grade is a : A+");
+               } else if (100 - grade >= 10) {
+                   System.out.println("Current grade is a : A-");
+               } else
+                   System.out.println("Current grade is a : A");
+           }
+
+//        System.out.print("Continue? (y/n): ");
+//        userChoice = myScanner.next();
+//        System.out.println();
+//    } while (!userChoice.equalsIgnoreCase("n"));
 
     }
+
+
 }
