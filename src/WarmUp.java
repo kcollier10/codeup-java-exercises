3f7ecc745824858c937d22cba2f3314e5723f1c4
Kristen Collier <kristencollier10@gmail.com>
2020-12-04 09:23:06 -0600

    warmup

---
+++ src/WarmUp.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class WarmUp {
+    public static void main (String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Welcome to 'PRINT THE VEGETABLE COUNT' app!\n");
+        System.out.print("How many vegetables do you have? ");
+        int count = myScanner.nextInt();
+        System.out.print("What kind of vegetable? (use a singular noun, e.g. 'carrot' vs. 'carrots'): ");
+        String veggie = myScanner.next();
+        System.out.printf("You have %d %ss!\n\n", count, veggie);
+    }
+}
