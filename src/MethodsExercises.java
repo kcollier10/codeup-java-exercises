1a038b55480c0c18413842914d9ca943d3126c4d
Kristen Collier <kristencollier10@gmail.com>
2020-12-04 15:04:58 -0600

    small updates on methods exercise

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class MethodsExercises {
 
+    Scanner myScanner;
+
     public static void main (String[] arg) {
 
         Scanner myScanner = new Scanner(System.in);
-        int x, y, sum, difference, product, quotient;
+        int x, y, sum, difference, product, quotient, modulus, userInput;
         System.out.println("Please enter two numbers.");
         x = myScanner.nextInt();
         y = myScanner.nextInt();
@@ -13,10 +15,17 @@ public class MethodsExercises {
         difference = Subtraction(x, y);
         product = Multiplication(x, y);
         quotient = Division(x, y);
+        modulus = Remainder(x, y);
         System.out.println("Sum: " + sum);
         System.out.println("Difference: " + difference);
         System.out.println("Product: " + product);
         System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + modulus);
+
+
+        System.out.println("Enter a number between 1 and 10.");
+        userInput = myScanner.nextInt();
+        getInteger(1, 10, userInput);
 
 
     }
@@ -34,7 +43,25 @@ public class MethodsExercises {
     }
 
     public static int Division(int x, int y) {
+        if (y == 0) {
+            System.out.println("Infinity");
+        }
         return x / y;
     }
+
+    public static int Remainder(int x, int y) {
+        return x % y;
+    }
+
+    public static void getInteger(int min, int max, int userInput) {
+
+        if (userInput >= min && userInput <= max) {
+            System.out.println("Valid number entered");
+        } else {
+            System.out.println("Please enter a different number.");
+        }
+    }
+
+
 
 }
