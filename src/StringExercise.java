e83928c0e34ae350c8f4a071afedc7a6d2f682a6
Kristen Collier <kristencollier10@gmail.com>
2020-12-04 10:04:07 -0600

    string exercise updates

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -4,6 +4,12 @@ public class StringExercise {
     public static void main (String [] args) {
         Scanner myScanner = new Scanner(System.in);
 
+        String lyrics = ("We don't need no education\nWe don't need no thought control");
+        System.out.println(lyrics);
+
+        String checkThisOut = ("Check \"this\" out!, \"s inside of \"s!");
+        System.out.println(checkThisOut);
+
 
     }
 }
