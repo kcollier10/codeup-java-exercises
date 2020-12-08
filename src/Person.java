1a2267429e191e8db9d27ade5b10231d3ed7ec92
Kristen Collier <kristencollier10@gmail.com>
2020-12-08 13:24:39 -0600

    short assignment #1

---
+++ src/Person.java
@@ -0,0 +1,43 @@
+public class Person {
+
+        // SLIDE 3
+//        public String firstName;
+//        public String lastName;
+
+
+        // SLIDE 4
+
+        //        // instance variables
+        public String firstName;
+        public String lastName;
+
+        // instance method
+        public String sayHello(char punctuation) {
+                return lastName + ", " + firstName + " says hi" + punctuation;
+        }
+
+        // instance variables can be accessed from inside a method
+        // no static keyword
+
+
+        // SLIDE 7
+//        public static long worldPopulation = 7_500_000_000L; // class property
+//        public String name;                                  // instance property
+//
+//        public static void main(String[] args) {
+//
+//                System.out.println(Person.worldPopulation);
+//                Person.worldPopulation -= 1;
+//                System.out.println(worldPopulation);
+//
+//
+//                Person theBestDrummerAlive = new Person();
+//                theBestDrummerAlive.name = "Neil Peart";
+//                Person.worldPopulation += 1; // accessing a static property
+//                // this also works, but is usually not a good idea
+//                System.out.println(theBestDrummerAlive.worldPopulation); // 7500000001
+//                // DONT DO THIS, it will not compile
+////                 System.out.println(Person.name);
+//        }
+
+}
