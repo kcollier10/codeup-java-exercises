7c6e4c0832ce03d76ed1b358a4d66695b5d0ff06
Kristen Collier <kristencollier10@gmail.com>
2020-12-10 11:15:54 -0600

    extra examples

--- src/PersonTest.java
+++ src/PersonTest.java
@@ -1,6 +1,9 @@
 public class PersonTest {
+
     public static void main (String[] args) {
+
         Person p1 = new Person("Kristen");
+
         p1.sayHello();
         p1.setName("K10");
 
@@ -20,13 +23,13 @@ public class PersonTest {
         // reassigning a reference to something else, so the container is still the same
         // would always return true because it always equals itself
 
-        Person person1 = new Person("John");
-        Person person2 = person1;
-        System.out.println(person1.getName());
-        System.out.println(person2.getName());
-        person2.setName("Jane");
-        System.out.println(person1.getName());
-        System.out.println(person2.getName());
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
         // becomes Jane for both names because they're still pointing to the same thing
 
     }
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -8,16 +8,16 @@ public class InputTest {
 
         Input input = new Input();
 
-        System.out.println("please enter a string:");
+//        System.out.println("please enter a string:");
         String userString = input.getString();
         System.out.println(userString);
-        System.out.println("please enter a value to test for true/false:");
+//        System.out.println("please enter a value to test for true/false:");
         Boolean userBoolean = input.yesNo();
         System.out.println(userBoolean);
-        input.getInt(1, 10);
-        System.out.println("please enter a value to test if entry is valid integer:");
-        input.getInt();
-        input.getDouble(1.0, 10.0);
+        System.out.println(input.getInt(1, 10));
+//        System.out.println("please enter a value to test if entry is valid integer:");
+//        input.getInt();
+        System.out.println(input.getDouble(1.0, 50.0));
 
 
 
