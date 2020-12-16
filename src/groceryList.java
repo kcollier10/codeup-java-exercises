63adb54118655be69cf19f3237f3734b14473b8e
Kristen Collier <kristencollier10@gmail.com>
2020-12-16 16:46:22 -0600

    updates

---
+++ src/groceryList.java
@@ -0,0 +1,14 @@
+import util.Input;
+
+public class groceryList {
+
+    public static void main(String[] args) {
+
+        System.out.println("Would you like to create a grocery list?");
+
+    }
+
+
+}
+
+
