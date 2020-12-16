a245e99d0e8aa10944e007df938c66e6c75eba0f
Kristen Collier <kristencollier10@gmail.com>
2020-12-16 15:26:55 -0600

    exercise 1 working

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -2,16 +2,36 @@ package grades;
 
 import java.util.HashMap;
 
-public class GradesApplication extends Student {
+public class GradesApplication {
 
-    public GradesApplication(String name) {
-        super(name);
-    }
+    static HashMap<String, Student> students = new HashMap<>();
 
     public static void main(String[] args) {
-        HashMap<String, Student> students = new HashMap<>();
 
-        System.out.println(students.keySet());
+        Student s1 = new Student("Kristen");
+        s1.addGrade(95);
+        s1.addGrade(96);
+        s1.addGrade(97);
+        s1.addGrade(98);
+
+        Student s2 = new Student("Chief");
+        s2.addGrade(100);
+        s2.addGrade(101);
+        s2.addGrade(102);
+        s2.addGrade(103);
+
+        Student s3 = new Student("Kitty");
+        s3.addGrade(97);
+        s3.addGrade(98);
+        s3.addGrade(99);
+        s3.addGrade(100);
+
+        Student s4 = new Student("Bob");
+        s4.addGrade(91);
+        s4.addGrade(92);
+        s4.addGrade(93);
+        s4.addGrade(94);
+
 
 
     }
--- src/grades/Student.java
+++ src/grades/Student.java
@@ -50,26 +50,26 @@ public class Student {
         // ArrayList<String> s1 = new ArrayList<>();
         // doesn't work
 
-        Student s1 = new Student("Kristen");
-        s1.addGrade(95);
-        s1.addGrade(96);
-        s1.addGrade(97);
-        s1.addGrade(98);
-        Student s2 = new Student("Chief");
-        s2.addGrade(100);
-        s2.addGrade(101);
-        s2.addGrade(102);
-        s2.addGrade(103);
-        Student s3 = new Student("Kitty");
-        s3.addGrade(97);
-        s3.addGrade(98);
-        s3.addGrade(99);
-        s3.addGrade(100);
-        Student s4 = new Student("Bob");
-        s4.addGrade(91);
-        s4.addGrade(92);
-        s4.addGrade(93);
-        s4.addGrade(94);
+//        Student s1 = new Student("Kristen");
+//        s1.addGrade(95);
+//        s1.addGrade(96);
+//        s1.addGrade(97);
+//        s1.addGrade(98);
+//        Student s2 = new Student("Chief");
+//        s2.addGrade(100);
+//        s2.addGrade(101);
+//        s2.addGrade(102);
+//        s2.addGrade(103);
+//        Student s3 = new Student("Kitty");
+//        s3.addGrade(97);
+//        s3.addGrade(98);
+//        s3.addGrade(99);
+//        s3.addGrade(100);
+//        Student s4 = new Student("Bob");
+//        s4.addGrade(91);
+//        s4.addGrade(92);
+//        s4.addGrade(93);
+//        s4.addGrade(94);
 
 
 
