40cc2cffb0b1700b9eded6e8856f4509f1453bf4
Kristen Collier <kristencollier10@gmail.com>
2020-12-17 10:44:35 -0600

    updates to exercise 2

--- src/GroceryListApp.java
+++ src/GroceryListApp.java
@@ -1,3 +1,5 @@
+import util.Input;
+
 import java.util.HashMap;
 
 public class GroceryListApp {
@@ -6,7 +8,41 @@ public class GroceryListApp {
 
         HashMap<String, groceryList> items = new HashMap<>();
 
-//        groceryList
+        groceryList fruits = new groceryList("Fruits");
+        groceryList veggies = new groceryList("Veggies");
+        groceryList petStuff = new groceryList("Pet Stuff");
+        groceryList toiletries = new groceryList("Toiletries");
+
+        fruits.addItem("bananas");
+        fruits.addItem("strawberries");
+        fruits.addItem("kiwi");
+        fruits.addItem("apples");
+        fruits.addItem("oranges");
+        items.put("Fruits", fruits);
+
+        veggies.addItem("tomatoes");
+        veggies.addItem("potatoes");
+        veggies.addItem("celery");
+        veggies.addItem("onions");
+        veggies.addItem("garlic");
+        items.put("Veggies", veggies);
+
+        petStuff.addItem("Chief food");
+        petStuff.addItem("Kitty food");
+        petStuff.addItem("Chief toy");
+        petStuff.addItem("Kitty toy");
+        petStuff.addItem("treats!");
+        items.put("Pet Stuff", petStuff);
+
+        toiletries.addItem("shampoo");
+        toiletries.addItem("conditioner");
+        toiletries.addItem("deodorant");
+        toiletries.addItem("brush");
+        toiletries.addItem("lotion");
+        items.put("Toiletries", toiletries);
+
+
+        Input userInput = new Input();
 
         System.out.println("Would you like to create a grocery list?");
 
