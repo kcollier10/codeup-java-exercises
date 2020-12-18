c9141c8bcbbc0b14594f77212c293155faf5286f
Kristen Collier <kristencollier10@gmail.com>
2020-12-18 12:20:18 -0600

    fileIO lecture

---
+++ src/fileIo/FileReader.java
@@ -0,0 +1,11 @@
+package fileIo;
+
+public class FileReader {
+
+    // properties for this class
+    // goal: read in a file and parse through it
+    private String directoryName; // i.e. 'data', 'src/fileIo'
+    private String fileName;      // i.s. 'day18.txt', 'jolts.txt'
+
+
+}
